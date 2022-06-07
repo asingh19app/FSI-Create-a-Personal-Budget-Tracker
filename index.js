cf329ec0a8e1f3f15d2bb2a072e5b9d5e7a2185c
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-06-07 18:16:06 -0400

    added js

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+/* when doing iterartion remember that the loops as a whole nee to be contained in {} or else all the loops will reieterarte continously 
+*/
+
+
+{
+for (let i = 0; i<weeklyExpenseQuestions.length; i++) {
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+
+weeklyExpenses = weeklyExpenses + answer
+
+}
+
+for (let i= 0; i< monthlyExpenseQuestions.length ; i++) {
+
+    let answer= parseFloat (window.prompt(monthlyExpenseQuestions[i]))
+
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for (let i = 0; i< annualExpenseQuestions.length; i++) {
+
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+
+    annualExpenses = annualExpenses + answer
+}
+
+}
